0cd2f12bea4420ea18ce3b1cd58ef4d35d673e50
Lucasmoidel <99782884+Lucasmoidel@users.noreply.github.com>
2024-06-26 11:36:29 -0700

    alot

--- final click/ViewModels/MainViewModel.cs
+++ final click/ViewModels/MainViewModel.cs
@@ -7,4 +7,7 @@ namespace final_click.ViewModels;
 public class MainViewModel : ViewModelBase
 {
     public string Greeting => "click to start";
+	public int AutoClick => 0;
+	public string AutoClickDefaultPice => "$20";
 }
+
--- final click/Views/MainView.axaml.cs
+++ final click/Views/MainView.axaml.cs
@@ -1,23 +1,66 @@
 ﻿using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Threading;
 using System;
+using System.Timers;
 
-namespace final_click.Views;
-
-public partial class MainView : UserControl
+namespace final_click.Views
 {
-    public MainView()
-    {
-        InitializeComponent();
-    }
-	int number = 0;
-	private void Click_button(object sender, RoutedEventArgs e)
+	public partial class MainView : UserControl
 	{
-		number++;
-		myTextBlock.Text = number.ToString();
-	}
-	private void AutoClicker(object sender, RoutedEventArgs e)
-	{
-		Console.WriteLine("auto clicker ");
+		bool GameStarted = false;
+		int number = 0;
+		int AutoClickNum = 0;
+		int AutoClickPrice = 20;
+		int AutoClickMult = 0;
+		int cps = 0;
+		Avalonia.Controls.TextBlock MainCount = null;
+		private static Timer aTimer;
+
+		public MainView()
+		{
+			InitializeComponent();
+			int cps = AutoClickNum * AutoClickMult;
+			aTimer = new System.Timers.Timer();
+			aTimer.Interval = 1000;
+			aTimer.Elapsed += Update;
+			aTimer.AutoReset = true;
+			aTimer.Enabled = true;
+		}
+
+		private void Click_button(object sender, RoutedEventArgs e)
+		{
+			number++;
+			myTextBlock.Text = "$" + number.ToString();
+			MainCount = myTextBlock;
+			GameStarted = true;
+		}
+
+		private void AutoClicker(object sender, RoutedEventArgs e)
+		{
+			if (number >= AutoClickPrice)
+			{
+				Console.WriteLine("auto clicker");
+				AutoClickNum++;
+				number -= AutoClickPrice;
+				AutoText.Text = AutoClickNum.ToString();
+				myTextBlock.Text = "$" + number.ToString();
+				AutoClickPrice++;
+				AutoClickPriceText.Text = "$" + AutoClickPrice.ToString();
+
+			}
+			
+		}
+
+		public void Update(object source, ElapsedEventArgs e)
+		{
+			if (GameStarted == true)
+			{
+				number += AutoClickNum;
+
+				// Dispatch UI update to the UI thread
+				Dispatcher.UIThread.InvokeAsync(() =>{ myTextBlock.Text = "$" + number.ToString(); });
+			}
+		}
 	}
 }
